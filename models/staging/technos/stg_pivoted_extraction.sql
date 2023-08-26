e50df70d5865575bfe48d30d3713fe91e281c8b9
Felita Donor <donorfelita@msn.com>
2023-08-26 20:12:41 +0000

    🦉 Simplify DAG by removing the sentences models: reduces considerably resulting number of rows

--- models/staging/technos/stg_pivoted_extraction.sql
+++ models/staging/technos/stg_pivoted_extraction.sql
@@ -8,22 +8,21 @@
 
 {%- set query_results = dbt_utils.get_query_results_as_dict(sql_statement) -%}
 
-with base_sentences_flatten as(
-    select * from {{ ref('stg_base_sentences_flatten') }}
+with jobs as (
+  select * from {{ ref("int_job_postings") }}
 ),
 
 mentions as (
     select
-        sentence_id,
-        job_id,
-        sentence_text,
+        id as job_id,
+        text,
         {%- for k in query_results['KEYWORD'] | list %}
         {%- set regex = query_results['KEYWORD_REGEX'][loop.index-1] %}
-        regexp_like(sentence_text, '.*({{ regex }}).*')
+        regexp_like(text, '.*({{ regex }}).*')
             as {{ k | replace(" ","_") | replace("|","_") | lower }} {{ "," if not loop.last}}
        {%- endfor %}
 
-    from base_sentences_flatten
+    from jobs
 )
 
 select * from mentions
