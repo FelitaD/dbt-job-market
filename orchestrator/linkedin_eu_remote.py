bc192c440b590d49304f2fda6ff4f18b4058245b
Donor <donorfelita@msn.com>
2023-09-04 06:51:28 +0200

    Write data from octoparse into snowflake

--- orchestrator/linkedin_eu_remote.py
+++ orchestrator/linkedin_eu_remote.py
@@ -7,8 +7,12 @@ import pandas as pd
 import glob
 import os
 import psycopg2
+import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
 
-from config.definitions import DB_STRING, PROJECT_PATH
+
+
+from config.definitions import DB_STRING, PROJECT_PATH, SNOWFLAKE_ACCOUNT, SNOWFLAKE_ROLE, SNOWFLAKE_USER, SNOWSQL_PWD, WAREHOUSE
 
 
 def extract_latest_crawl(spider: str = 'linkedin_eu_remote'):
@@ -17,11 +21,23 @@ def extract_latest_crawl(spider: str = 'linkedin_eu_remote'):
     return latest_file, pd.read_csv(latest_file)
 
 def transform_data(data: pd.DataFrame = None):
-    data.rename(str.lower, axis='columns', inplace=True)
+    """
+    Processes raw data before inserting into database.
+    """
+    # Rename columns to small letters
+    data = data.rename(str.lower, axis='columns')
+
+    # Drop columns that don't respect NOT NULL constraint
+    data = data.dropna(subset=['url', 'text'])
+
+    # Remove whitespaces in string columns
     df_obj = data.select_dtypes(['object'])
     data[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
+
+    # Change column with date information (eg. "1 week ago") to datetime objects
     data['created_at'] = data['date_posted'].apply(lambda x: parse_created_at(x))
     data['created_at'] = pd.to_datetime(data['created_at']).apply(lambda x: x.strftime('%Y-%m-%d'))
+
     return data[['url', 'title', 'company', 'location', 'text', 'created_at']]
 
 @staticmethod
@@ -38,17 +54,26 @@ def parse_created_at(series):
             if str(n) in series:
                 return now - timedelta(weeks=n)
 
-def insert_data(data, db: sqlalchemy.engine.base.Engine, query: str, parameters: dict):
-    log_headline: str = "insert_data() ::"
-    stmt = sqlalchemy.text(query)
 
-    try:
-        with db.connect() as conn:
-            conn.execute(stmt, parameters=parameters)
-            conn.commit()
-        print(f"{log_headline} OK inserted data ")
-    except Exception as e:
-        print(f"{log_headline} Error {e}")
+def insert_data_snowflake(data):
+    """
+    Automates the workflow of uploading data to a stage and then copying into a Snowflake table.
+    """
+    data.rename(str.upper, axis='columns', inplace=True)
+    cnx = snowflake.connector.connect(
+        account=SNOWFLAKE_ACCOUNT,
+        user=SNOWFLAKE_USER,
+        password=SNOWSQL_PWD,
+        database='RAW',
+        schema='PUBLIC',
+        role=SNOWFLAKE_ROLE,
+        warehouse=WAREHOUSE
+    )
+    success, nchunks, nrows, output = write_pandas(cnx, data, 'JOB_POSTINGS')
+    print(f'Insertion: {success}, '
+          f'Number of chunks inserted: {nchunks}, '
+          f'Number of rows inserted: {nrows}, '
+          f'Inserted data: {output}')
 
 
 def linkedin_eu_remote():
@@ -60,19 +85,8 @@ def linkedin_eu_remote():
     transformed = transform_data(raw)
     # logger.info('\n  ------- Tranformed data: ------- \n')
 
-    # logger.info('\n  ------- Loading data... ------- \n')
-    engine = create_engine(DB_STRING, echo=True)
-    query = "INSERT INTO raw_jobs (url, title, company, location, text, created_at) VALUES (:url, :title, :company, :location, :text, :created_at)"
-    for i in range(len(transformed)):
-        parameters = {
-            'url': transformed['url'].iloc[i],
-            'title': transformed['title'].iloc[i],
-            'company': transformed['company'].iloc[i],
-            'location': transformed['location'].iloc[i],
-            'text': transformed['text'].iloc[i],
-            'created_at': transformed['created_at'].iloc[i]
-            }
-        insert_data(data=transformed, db=engine, query=query, parameters=parameters)
+    # logger.info('\n  ------- Inserted data: ------- \n')
+    insert_data_snowflake(transformed)
 
 if __name__ == '__main__':
     linkedin_eu_remote()
