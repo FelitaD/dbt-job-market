a8ffc79961d410abbdfde66b616b8d83a0661c20
Felita Donor <donorfelita@msn.com>
2023-09-18 19:15:51 +0000

    Adapt query to BigQuery: change regexp function and lower dict field names

--- transformation/models/staging/stg_pivoted_extraction.sql
+++ transformation/models/staging/stg_pivoted_extraction.sql
@@ -1,12 +1,12 @@
 {% set sql_statement %}
     select 
         keyword, 
-        keyword_regex, 
-        keyword_category 
-    from {{ ref('stg_base_keywords') }}
+        keyword_regex
+    from {{ ref('technos') }}
 {% endset %}
 
 {%- set query_results = dbt_utils.get_query_results_as_dict(sql_statement) -%}
+
 
 with jobs as (
   select * from {{ ref("stg_job_postings") }}
@@ -16,10 +16,10 @@ mentions as (
     select
         id as job_id,
         text,
-        {%- for k in query_results['KEYWORD'] | list %}
-        {%- set regex = query_results['KEYWORD_REGEX'][loop.index-1] %}
-        regexp_like(text, $$.*[\s,.\n]({{ regex }})[\s,.\n].*$$, 'i')
-            as {{ k | replace(" ","_") | replace("|","_") | lower }} {{ "," if not loop.last}}
+        {%- for k in query_results['keyword'] | list %}
+        {%- set regex = query_results['keyword_regex'][loop.index-1] %}
+        regexp_contains(text, r'(?i).*[\s,.\n]({{ regex }})[\s,.\n].*')
+            as `{{ k | replace(" ","_") | replace("|","_") | lower }}` {{ "," if not loop.last}}
        {%- endfor %}
 
     from jobs
