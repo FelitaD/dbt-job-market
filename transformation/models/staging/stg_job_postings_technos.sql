354b1238cc879186f0d85045068ce8068dc4b292
Felita Donor <donorfelita@msn.com>
2023-09-19 07:42:33 +0000

    Modify replace function with regexp_replace and reorder joins

--- transformation/models/staging/stg_job_postings_technos.sql
+++ transformation/models/staging/stg_job_postings_technos.sql
@@ -1,34 +1,50 @@
-{{ config(materialized='table') }}
-
 with unpivoted_numbered as (
-    select job_id, keyword, text from {{ ref('stg_unpivoted_numbered') }}
+    select 
+        id, 
+        regexp_replace(regexp_replace(key, r'^(k_)', ''), r'_', ' ') as keyword
+    from {{ ref('stg_unpivoted_numbered') }}
 ),
 
 job_postings as (
-    select id, url, title, company, location, contract, industry, remote, created_at from {{ ref('stg_job_postings') }}
+    select 
+        id, 
+        company, 
+        contract, 
+        created_at,
+        industry, 
+        location, 
+        title, 
+        remote, 
+        text, 
+        url, 
+    from {{ ref('stg_job_postings') }}
 ),
 
 technos as (
-    select name as techno, keyword
+    select 
+        keyword,
+        name as techno
     from {{ ref('technos') }}
 ),
 
-join_job_postings as (
-    select  
-        *,
-        lower(replace(u.keyword, '_', ' ')) as keyword
+job_postings_technos as (
+    select 
+        id, 
+        company,
+        contract,
+        created_at,
+        industry,
+        location,
+        remote, 
+        techno,
+        text,
+        title, 
+        url,
     from unpivoted_numbered u
     join job_postings j
-    on u.job_id = j.id
-),
-
-job_postings_technos as (
-    select *
-    from join_job_postings j
+    using(id)
     join technos t
     using(keyword)
 )
 
-select id, title, company, techno,
-        location, remote, contract, industry, text, url, created_at
-from job_postings_technos
+select * from job_postings_technos
