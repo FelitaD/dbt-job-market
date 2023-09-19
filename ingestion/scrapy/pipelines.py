eca5234c158cfb8701044bfccad110688774129f
Donor <donorfelita@msn.com>
2023-09-19 09:49:34 +0200

    Rename raw jobs table in bigquery

--- ingestion/scrapy/pipelines.py
+++ ingestion/scrapy/pipelines.py
@@ -32,7 +32,7 @@ class JobsCrawlerPipeline:
         
         with engine.connect() as connection:
             stmt = text(
-                "MERGE job_market.job_postings target "
+                "MERGE job_market.raw_job_postings target "
                 "USING ( SELECT :url AS new_url, :created_at AS new_created_at) source "
                 "ON target.url = source.new_url "
                 "WHEN MATCHED THEN "
