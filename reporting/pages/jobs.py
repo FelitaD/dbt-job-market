6925c395da689481a1481b908add9f47e6cef796
Donor <donorfelita@msn.com>
2023-09-21 17:18:54 +0200

    Delete apply functions from pandas Styler

--- reporting/pages/jobs.py
+++ reporting/pages/jobs.py
@@ -16,15 +16,17 @@ tab_charts, tab_data, tab_raw_data = st.tabs(['Charts', 'Data', 'Raw data'])
 with tab_data:
     relevant_jobs = run_query(relevant_jobs_stmt)
     relevant_jobs_df = pd.DataFrame(relevant_jobs)
-    print(relevant_jobs_df)
 
     st.dataframe(
         filter_dataframe(relevant_jobs_df).style
-        .apply(highlight_title, axis=1, subset=['title'])
-        .apply(highlight_quant_column, subset=['rating'])
         .format('{:.1f}', subset=['rating'])
-        .format('{:.0f}', subset=['reviews']),
+        .format('{:.0f}', subset=['reviews', 'total_score', 'rating_score']),
         column_config={
+            'total_score': st.column_config.Column(width='small'),
+            'is_relevant_score': st.column_config.Column(width='small'),
+            'seniority_score': st.column_config.Column(width='small'),
+            'is_same_glassdoor_score': st.column_config.Column(width='small'),
+            'rating_score': st.column_config.Column(width='small'),
             "stack": st.column_config.ListColumn(width='medium'),
             "text": st.column_config.TextColumn(width='medium'),
             "size": st.column_config.TextColumn(width='small'),
@@ -33,3 +35,4 @@ with tab_data:
         },
         height=800
     )
+
