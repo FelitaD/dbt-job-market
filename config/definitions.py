cfaecfeb125827d5035625d4200e3b8f3d0c71d9
Donor <donorfelita@msn.com>
2023-09-26 22:14:05 +0200

    Move sql queries to definitions

--- config/definitions.py
+++ config/definitions.py
@@ -19,3 +19,35 @@ all_data_columns = ['company_name', 'name', 'url', 'industry', 'headquarters', '
                     'company', 'contract', 'created_at', 'industry_1', 'location', 'remote',
                     'stack', 'text', 'title', 'url_1', 'id_1', 'is_relevant',
                     'is_same_glassdoor', 'seniority_score', 'rating_score', 'total_score']
+
+# Google SQL queries
+
+companies_stmt = """
+    select *
+    from `job_market.companies`; 
+"""
+
+all_data_stmt = """
+    select * 
+    from `job_market.companies` c
+    join `job_market.jobs` j 
+    on c.company_name = j.company
+    join `job_market.scores` s
+    on j.id = s.id
+    where is_relevant = 1
+    order by total_score desc, created_at desc;
+"""
+
+technos_stmt = """
+select distinct techno, total, category, subcategory, description
+from (
+    select count(*) as total, techno from `job_market.stg_job_postings_technos` group by techno
+    ) as j
+join `job_market.technos` t
+on t.name = j.techno
+order by total desc;
+"""
+
+sankey_stmt = """
+select * from `job_market.sankey_data`;
+"""
