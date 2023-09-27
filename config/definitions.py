7ae07c02d07d9baca1c2935eed106080b3e960c0
Donor <donorfelita@msn.com>
2023-09-27 18:48:24 +0200

    Delete unused environment variables

--- config/definitions.py
+++ config/definitions.py
@@ -3,17 +3,6 @@ from pathlib import Path
 
 PROJECT_PATH = Path(__file__).parent.parent.resolve()
 
-JOB_MARKET_DB_PWD = os.environ['JOB_MARKET_DB_PWD']
-JOB_MARKET_DB_USER = os.environ['JOB_MARKET_DB_USER']
-DB_STRING = f"postgresql://{JOB_MARKET_DB_USER}:{JOB_MARKET_DB_PWD}@localhost:5432/job_market"
-SNOWSQL_PWD = os.getenv('SNOWSQL_PWD')
-WAREHOUSE = os.getenv('WAREHOUSE')
-SNOWFLAKE_ACCOUNT = os.getenv('SNOWFLAKE_ACCOUNT')
-SNOWFLAKE_ROLE = os.getenv('SNOWFLAKE_ROLE')
-SNOWFLAKE_USER = os.getenv('SNOWFLAKE_USER')
-OCTOPARSE_USER = os.getenv('OCTOPARSE_USER')
-OCTOPARSE_PWD = os.getenv('OCTOPARSE_PWD')
-
 all_data_columns = ['company_name', 'name', 'url', 'industry', 'headquarters', 'rating',
                     'company_size', 'reviews_count', 'jobs_count', 'salaries_count', 'id',
                     'company', 'contract', 'created_at', 'industry_1', 'location', 'remote',
