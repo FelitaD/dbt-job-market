758094b742d8d34616c9ede7d4b05b1eccb2a082
Donor <donorfelita@msn.com>
2023-09-27 17:14:37 +0200

    Refactor tabs

--- reporting/home.py
+++ reporting/home.py
@@ -14,10 +14,11 @@ from reporting.charts.treemap_technos import treemap_technos
 from reporting.dataframes.technos_df import create_technos_st_df
 from reporting.helpers.filter_dataframe import DataframeFilter
 from reporting.dataframes.job_board import create_job_board
+from reporting.dataframes.all_jobs_df import all_jobs_df
 from reporting.dataframes.companies_df import create_companies_st_df
 from reporting.charts.scatter_companies import scatter_companies
 
-tab_job_market, tab_me = st.tabs(['Data Engineering Job Market', 'About me'])
+tab_job_market, tab_charts, tab_data = st.tabs(['Data Engineering Job Board', 'Charts', 'Data'])
 
 # Initialize dataframe filterer
 df_filter = DataframeFilter()
@@ -58,7 +59,6 @@ with st.sidebar:
             remote_filter=remote_filter)
 
 with tab_job_market:
-    st.subheader("Job Board")
 
     if add_filters:
         st.write(f'Results contain {len(filtered_df)} rows')
@@ -67,15 +67,25 @@ with tab_job_market:
         st.write(f'Results contain {len(df_filter.df)} rows')
         create_job_board(df_filter.df)  # Unfiltered dataframe
 
-    st.subheader("Charts")
-    st.plotly_chart(sankey_fig)
-    st.plotly_chart(treemap_technos)
-    st.plotly_chart(scatter_companies)
-    # chart time series scores
+with tab_charts:
 
-    st.subheader("Data")
+    st.subheader('Number of job postings during pipeline transformations')
+    st.plotly_chart(sankey_fig)
+
+    st.subheader('Occurence of technologies in job descriptions')
+    st.plotly_chart(treemap_technos)
+
+    st.subheader("Companies' Glassdoor rating, per number of reviews and company size")
+    st.plotly_chart(scatter_companies)
+
+with tab_data:
+
+    st.subheader('All jobs (not relevant included)')
+    st.dataframe(all_jobs_df)
+
+    st.subheader("Technologies")
     create_technos_st_df()
+
+    st.subheader("Companies")
     create_companies_st_df()
 
-with tab_me:
-    st.markdown("Timeline")
