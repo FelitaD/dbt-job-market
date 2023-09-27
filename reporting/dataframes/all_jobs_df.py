9382fbf24bac056567b035d9601457750dd42014
Donor <donorfelita@msn.com>
2023-09-27 17:14:52 +0200

    Add all jobs dataframe

--- reporting/dataframes/all_jobs_df.py
+++ reporting/dataframes/all_jobs_df.py
@@ -1,35 +1,13 @@
-"""Produces the companies' dataframe in Streamlit.
+"""Produces all jobs (not relevant included) dataframe in Streamlit.
 
-First, a copy of the companies table is created as a dataframe.
-The `st.data_editor` module customises the columns in the UI.
+Data stems from the same query as `job_board.py` but without
+the `relevant = 1` condition. Hence it doesn't create another st.editor.
 """
 
 import pandas as pd
 import streamlit as st
 
 from reporting.helpers.run_query import run_query
-from config.definitions import companies_stmt
+from config.definitions import all_jobs_stmt
 
-companies_df = pd.DataFrame(run_query(companies_stmt))
-
-
-def create_companies_st_df() -> st.data_editor:
-    """Creates the companies dataframe in the UI.
-
-    Returns:
-        An instance of st.data_editor.
-    """
-    return st.data_editor(companies_df.sort_values(by=['company_name']),
-                          use_container_width=True,
-                          column_config={
-                              "company_name": st.column_config.TextColumn('name (listing)'),
-                              "name": st.column_config.TextColumn('name (glassdoor)'),
-                              "url": st.column_config.LinkColumn('url', width='small'),
-                              "industry": st.column_config.Column(),
-                              "headquarters": st.column_config.Column(),
-                              "rating": st.column_config.Column(),
-                              "company_size": st.column_config.NumberColumn('size mean'),
-                              "reviews_count": st.column_config.NumberColumn(),
-                              "jobs_count": st.column_config.NumberColumn(),
-                              "salaries_count": st.column_config.NumberColumn(),
-                          })
+all_jobs_df = pd.DataFrame(run_query(all_jobs_stmt))
