3f948a1e9f81164c5030fffe9de0df682aa82944
Donor <donorfelita@msn.com>
2023-09-28 09:52:13 +0200

    Add streamlit elements sample code

--- home.py
+++ home.py
@@ -82,6 +82,36 @@ with tab_job_board:
 
 with tab_charts:
     # TODO: modify charts to be used in Streamlit Elements
+    with elements("dashboard"):
+        # Build a default layout for every element to include in the dashboard
+        layout = [
+            # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
+            dashboard.Item("first_item", 0, 0, 2, 2),
+            dashboard.Item("second_item", 2, 0, 2, 2, isDraggable=False, moved=False),
+            dashboard.Item("third_item", 0, 2, 1, 1, isResizable=False),
+        ]
+
+        # Next, create a dashboard layout using the 'with' syntax. It takes the layout
+        # as first parameter, plus additional properties you can find in the GitHub links below.
+
+        with dashboard.Grid(layout):
+            mui.Paper("First item", key="first_item")
+            mui.Paper("Second item (cannot drag)", key="second_item")
+            mui.Paper("Third item (cannot resize)", key="third_item")
+
+        # If you want to retrieve updated layout values as the user move or resize dashboard items,
+        # you can pass a callback to the onLayoutChange event parameter.
+
+        def handle_layout_change(updated_layout):
+            # You can save the layout in a file, or do anything you want with it.
+            # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
+            print(updated_layout)
+
+        with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
+            mui.Paper("First item", key="first_item")
+            mui.Paper("Second item (cannot drag)", key="second_item")
+            mui.Paper("Third item (cannot resize)", key="third_item")
+
     st.subheader('Number of job postings during pipeline transformations')
     st.plotly_chart(sankey_fig)
 
